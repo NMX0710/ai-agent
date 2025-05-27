f261f3d343681104d9b538b0d9f5d3d21892476f
NMX0710 <elvanie0710@gmail.com>
2025-05-27 17:15:06 +0800

    finish indexing

--- app/rag/recipe_app_document_loader.py
+++ app/rag/recipe_app_document_loader.py
@@ -4,33 +4,48 @@ from typing import List
 
 from langchain_community.document_loaders import UnstructuredMarkdownLoader
 from langchain_core.documents import Document
+from langchain_core.embeddings import Embeddings
+from langchain_core.vectorstores import InMemoryVectorStore
 
-class RecipeAppDocumentLoader:
+
+
+# 自动拼出绝对路径：以当前 Python 文件为基准
+base_path = Path(__file__).parent.parent.parent
+folder_path_default = base_path / "resources" / "documents"
+
+def indexing(folder_path: Path = folder_path_default, mode: str = "elements", embeddings: Embeddings = None) -> InMemoryVectorStore:
     """
+    Loading Documents
     批量加载本地 Markdown 文档并返回 LangChain Document 对象列表
     """
-    def __init__(self, folder_path:str = None, mode:str = "elements"):
-        """
-        :param folder_path: Markdown 文档所在的文件夹路径
-        :param mode: "single" 将整篇文档合并为一个 Document；
-                     "elements" 会保留不同段落/结构元素为多个 Document。
-        """
-        if folder_path is None:
-            # 自动拼出绝对路径：以当前 Python 文件为基准
-            base_path = Path(__file__).parent.parent.parent  # 到项目根目录
-            folder_path = base_path / "resources" / "documents"
-        self.folder_path = Path(folder_path)
-        self.mode = mode
+    if not folder_path.exists() or not folder_path.is_dir():
+        raise ValueError(f"指定路径无效: {folder_path}")
 
-    def load_md(self) -> List[Document]:
-        if not self.folder_path.exists() or not self.folder_path.is_dir():
-            raise ValueError(f"指定路径无效: {self.folder_path}")
-        all_documents = []
+    all_documents = []
+    for file in folder_path.glob("*.md"):
+        loader = UnstructuredMarkdownLoader(str(file), mode=mode)
+        # 因为我们的mode用的是element，md file里面每一个元素都会是一个document
+        docs = loader.load()
+        all_documents.extend(docs)
 
-        for file in self.folder_path.glob("*.md"):
-            loader = UnstructuredMarkdownLoader(str(file), mode = self.mode)
-            # 因为我们的mode用的是element，md file里面每一个元素都会是一个document
-            docs = loader.load()
-            all_documents.extend(docs)
-        return all_documents
+    """
+    Split Documents(Optional)
+    Split documents if the size of each document is too large
+    """
+
+    #text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    #all_splits = text_splitter.split_documents(docs)
+
+    """
+    Storing Documents
+    """
+    vector_store = InMemoryVectorStore(embeddings)
+    _ = vector_store.add_documents(documents=all_documents)
+    return vector_store
+
+
+
+
+
+
 
